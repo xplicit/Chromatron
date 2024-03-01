cb6ab55fa66b60cd727b9c1a0a9126ca1698906e
alexey.shevin <alexey.shevin@emu.codes>
2024-03-01 10:14:21 +0700

    Chromely demos added to Chromatron

---
+++ Demos/ChromatronDemos/BasicHTMLDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Chromely;
+using Chromely.Core;
+using Chromely.Core.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text.Json;
+
+namespace BasicHTMLDemo
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var config = DefaultConfiguration.CreateForRuntimePlatform();
+            config.StartUrl = "local://index.html";
+
+            AppBuilder
+            .Create()
+            .UseConfig<DefaultConfiguration>(config)
+            .UseApp<DemoApp>()
+            .Build()
+            .Run(args);
+        }
+    }
+
+    public class DemoApp : ChromelyBasicApp
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            base.ConfigureServices(services);
+            services.AddLogging(configure => configure.AddConsole());
+
+            RegisterControllerAssembly(services, typeof(DemoApp).Assembly);
+        }
+    }
+}
---
+++ Demos/ChromatronDemos/ReactDemo/reactapp/src/components/MainPage.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState, useMemo } from "react";
+import styled from "styled-components";
+import { restClient } from "polygon.io";
+
+const MainBlock = styled.div`
+    display: block;
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+`;
+
+const Header = styled.h1`
+    display: block;
+    font-size: 36px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 800;
+    font-style: italic;
+    text-align: center;
+`;
+
+const UsdPair = styled.div`
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    position: absolute;
+    top: 15%;
+    left: 15vw;
+`;
+
+const CryptoCurrency = styled.div`
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin-left: 60%;
+    position: absolute;
+    top: 15%;
+    left: 15vw;
+`;
+
+const StyledH2 = styled.h2`
+    font-size: 16px;
+    font-style: italic;
+    font-weight: bolder;
+    padding: 2px;
+`;
+
+const StyledSpan = styled.span`
+    font-size: 16px;
+    font-style: italic;
+    padding: 2px;
+`;
+
+export const MainPage = () => {
+
+    const [usdData, setUsdData] = useState();
+    const [cryptoData, setCryptoData] = useState();
+
+    const apiKey = "kAnzWCwy5ZXzPHYZ2DHmmO7LBIA1y62G";
+    const rest = restClient(apiKey);
+
+    const formattedCurrentDate = useMemo(() => {
+        const newDate = new Date();
+        const year = newDate.getFullYear();
+        const month = newDate.getMonth() + 1;
+        const d = newDate.getDate();
+
+        return `${year}-${month.toString().padStart(2, '0')}-${d.toString().padStart(2, '0')}`;
+    }, []);
+
+    const getUsdData = async () => {
+        await rest.stocks.aggregates("C:EURUSD", 1, "day", "2024-02-01", `${formattedCurrentDate}`).then((data) => {
+            setUsdData(data.results[0]);
+        }).catch(e => {
+            console.error('An error happened:', e);
+        });
+    };
+
+    const getCryptoData = async () => {
+        await rest.stocks.aggregates("X:BTCUSD", 1, "day", "2024-02-01", `${formattedCurrentDate}`).then((data) => {
+            setCryptoData(data.results[0]);
+        }).catch(e => {
+            console.error('An error happened:', e);
+        });
+    };
+
+    useEffect(() => {
+        getUsdData();
+        getCryptoData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <MainBlock className="main-page">
+            <Header>
+                Demo App With Poligon IO data.
+            </Header>
+            {usdData !== undefined && (
+                <UsdPair>
+                    <StyledH2>EUR/USD</StyledH2>
+                    <StyledSpan>C: {usdData.c}</StyledSpan>
+                    <StyledSpan>Close: {usdData.close}</StyledSpan>
+                    <StyledSpan>H: {usdData.h}</StyledSpan>
+                    <StyledSpan>High: {usdData.high}</StyledSpan>
+                    <StyledSpan>L: {usdData.l}</StyledSpan>
+                    <StyledSpan>Low: {usdData.low}</StyledSpan>
+                    <StyledSpan>O: {usdData.o}</StyledSpan>
+                    <StyledSpan>Open: {usdData.open}</StyledSpan>
+                </UsdPair>
+            )}
+            {cryptoData !== undefined && (
+                <CryptoCurrency>
+                    <StyledH2>BTC/USD</StyledH2>
+                    <StyledSpan>C: {cryptoData.c}</StyledSpan>
+                    <StyledSpan>Close: {cryptoData.close}</StyledSpan>
+                    <StyledSpan>H: {cryptoData.h}</StyledSpan>
+                    <StyledSpan>High: {cryptoData.high}</StyledSpan>
+                    <StyledSpan>L: {cryptoData.l}</StyledSpan>
+                    <StyledSpan>Low: {cryptoData.low}</StyledSpan>
+                    <StyledSpan>O: {cryptoData.o}</StyledSpan>
+                    <StyledSpan>Open: {cryptoData.open}</StyledSpan>
+                </CryptoCurrency>
+            )}
+        </MainBlock>
+    );
+};
